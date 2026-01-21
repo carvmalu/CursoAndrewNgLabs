36bb77e8855e370c74e7a8480ad35ce53d6a269c
carvmalu <weull@MALU>
2026-01-21 16:26:58 -0300

    Lab principal c2 semana 1 finalizado

--- C2 - Advanced Learning Algorithms/week1/C2W1A1/public_tests.py
+++ C2 - Advanced Learning Algorithms/week1/C2W1A1/public_tests.py
@@ -1,6 +1,6 @@
 # UNIT TESTS
-from tensorflow.keras.activations import sigmoid
-from tensorflow.keras.layers import Dense
+#from tensorflow.keras.activations import sigmoid
+#from tensorflow.keras.layers import Dense
 
 import numpy as np
 
